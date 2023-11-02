6fe59d8e003d939545bb2e010067a2d466a74bfd
Thaker13 <149239169+Thaker13@users.noreply.github.com>
2023-11-02 00:17:42 +0000

    Add files via upload

---
+++ NumberPositiveNegative.java
@@ -0,0 +1,22 @@
+package numberpositivenegative;
+
+public class NumberPositiveNegative {
+
+    public static void main(String[] args) {
+
+        int value = 3;
+        if (value < 0) {
+
+            System.out.println(value + "is Negative number");
+
+        } else if (value > 0) {
+
+            System.out.println(value + "is positive number");
+
+        } else {
+
+            System.out.println(value + "is Zero number");
+        }
+    }
+
+}
